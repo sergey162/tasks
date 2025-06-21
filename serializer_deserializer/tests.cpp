5609a5f6069b6a087139742f373ea16a5fb04b4b
sergey162 <144516112+sergey162@users.noreply.github.com>
2025-06-21 21:55:44 +0300

    Update tests.cpp

--- serializer_deserializer/tests.cpp
+++ serializer_deserializer/tests.cpp
@@ -1,146 +1,118 @@
+#include <gtest/gtest.h>
 #include <sstream>
-#include <cassert>
 #include <iostream>
 #include "serializer.hpp"
 #include "deserializer.hpp"
 #include "data.hpp"
 
-// Тест успешной сериализации/десериализации
-void test_basic_serialization() {
+TEST(SerializationTest, Basic) {
     Data x{1, true, 2};
     std::stringstream stream;
     
-    // Сериализация
-    Serializator serializer(stream);
+    Serializer serializer(stream);
     Error err = serializer.Save(x);
-    assert(err == Error::NoError);
+    ASSERT_EQ(err, Error::NoError);
     
-    // Десериализация
     Data y{0, false, 0};
     Deserializer deserializer(stream);
     err = deserializer.Load(y);
     
-    assert(err == Error::NoError);
-    assert(x.a == y.a);
-    assert(x.b == y.b);
-    assert(x.c == y.c);
-    
-    std::cout << "Basic serialization test passed\n";
+    ASSERT_EQ(err, Error::NoError);
+    EXPECT_EQ(x.a, y.a);
+    EXPECT_EQ(x.b, y.b);
+    EXPECT_EQ(x.c, y.c);
 }
 
-// Тест поврежденного потока (пустой)
-void test_corrupted_stream_empty() {
+TEST(SerializationTest, CorruptedStreamEmpty) {
     Data y{0, false, 0};
     std::stringstream empty_stream;
     
     Deserializer deserializer(empty_stream);
     Error err = deserializer.Load(y);
     
-    assert(err == Error::CorruptedArchive);
-    std::cout << "Empty stream test passed\n";
+    ASSERT_EQ(err, Error::CorruptedArchive);
 }
 
-// Тест неверного формата boolean
-void test_invalid_bool_format() {
+TEST(SerializationTest, InvalidBoolFormat) {
     Data y{0, false, 0};
     std::stringstream stream;
-    stream << "1 unknown 2 ";  // Второе значение - неверный boolean
+    stream << "1 unknown 2 ";
     
     Deserializer deserializer(stream);
     Error err = deserializer.Load(y);
     
-    assert(err == Error::CorruptedArchive);
-    std::cout << "Invalid boolean format test passed\n";
+    ASSERT_EQ(err, Error::CorruptedArchive);
 }
 
-// Тест недостающих данных
-void test_missing_data() {
+TEST(SerializationTest, MissingData) {
     Data x{1, true, 2};
     std::stringstream stream;
     
-    // Сериализация только первых двух полей
-    Serializator serializer(stream);
+    Serializer serializer(stream);
     Error err = serializer.Save(x);
-    assert(err == Error::NoError);
+    ASSERT_EQ(err, Error::NoError);
     
-    // Урезаем поток
-    std::string partial_data = stream.str().substr(0, stream.str().find_last_of(' '));
+    std::string partial_data = stream.str();
+    size_t last_space = partial_data.find_last_of(' ');
+    partial_data = partial_data.substr(0, last_space);
+    
     std::stringstream modified_stream(partial_data);
-    
     Data y{0, false, 0};
     Deserializer deserializer(modified_stream);
     err = deserializer.Load(y);
     
-    assert(err == Error::CorruptedArchive);
-    std::cout << "Missing data test passed\n";
+    ASSERT_EQ(err, Error::CorruptedArchive);
 }
 
-// Тест лишних данных
-void test_extra_data() {
+TEST(SerializationTest, ExtraData) {
     Data x{1, true, 2};
     std::stringstream stream;
     
-    // Сериализация
-    Serializator serializer(stream);
+    Serializer serializer(stream);
     Error err = serializer.Save(x);
-    assert(err == Error::NoError);
+    ASSERT_EQ(err, Error::NoError);
     
-    // Добавляем лишние данные
-    stream << "extra_data";
+    stream << " extra_data";
     
     Data y{0, false, 0};
     Deserializer deserializer(stream);
     err = deserializer.Load(y);
     
-    assert(err == Error::CorruptedArchive);
-    std::cout << "Extra data test passed\n";
+    ASSERT_EQ(err, Error::CorruptedArchive);
 }
 
-// Тест неверного формата числа
-void test_invalid_number_format() {
+TEST(SerializationTest, InvalidNumberFormat) {
     Data y{0, false, 0};
     std::stringstream stream;
-    stream << "not_a_number true 2 ";  // Первое значение - не число
+    stream << "not_a_number true 2";  
     
     Deserializer deserializer(stream);
     Error err = deserializer.Load(y);
     
-    assert(err == Error::CorruptedArchive);
-    std::cout << "Invalid number format test passed\n";
+    ASSERT_EQ(err, Error::CorruptedArchive);
 }
 
-// Тест граничных значений
-void test_edge_values() {
+TEST(SerializationTest, EdgeValues) {
     Data x{0, false, 0};
-    x.a = UINT64_MAX;
-    x.c = UINT64_MIN;
+    x.a = std::numeric_limits<uint64_t>::max();
+    x.c = 0; 
     
     std::stringstream stream;
-    Serializator serializer(stream);
+    Serializer serializer(stream);
     Error err = serializer.Save(x);
-    assert(err == Error::NoError);
+    ASSERT_EQ(err, Error::NoError);
     
-    Data y{0, true, 0};
+    Data y{0, true, 1};
     Deserializer deserializer(stream);
     err = deserializer.Load(y);
     
-    assert(err == Error::NoError);
-    assert(x.a == y.a);
-    assert(x.b == y.b);
-    assert(x.c == y.c);
-    
-    std::cout << "Edge values test passed\n";
+    ASSERT_EQ(err, Error::NoError);
+    EXPECT_EQ(x.a, y.a);
+    EXPECT_EQ(x.b, y.b);
+    EXPECT_EQ(x.c, y.c);
 }
 
-int main() {
-    test_basic_serialization();
-    test_corrupted_stream_empty();
-    test_invalid_bool_format();
-    test_missing_data();
-    test_extra_data();
-    test_invalid_number_format();
-    test_edge_values();
-    
-    std::cout << "All tests passed successfully!\n";
-    return 0;
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
